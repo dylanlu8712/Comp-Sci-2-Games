081a9cb97be345b6c326fcbd60bee9943f51964b
Patrick <pwblais@icloud.com>
2016-12-12 15:19:13 -0500

    setup1

--- src/dankcode/TTT.java
+++ src/dankcode/TTT.java
@@ -1,10 +1,26 @@
-package dankcode;
+ package dankcode;
 
 public class TTT {
+	
+	static int topleft, topmid, topright, midleft, midmid, midright, botleft, botmid, botright;
 
 	public static void main(String[] args) {
 		//Alright Dylan, clutch up.
-
+		
+		//Can't use arrays or anything so look above for the variables for the grid
+		
+	}
+	
+	public void initialize() {
+		//set up the field
+	}
+	
+	public void grid() {
+		
+	}
+	
+	public boolean isWinner() {
+		return;
 	}
 
 }
